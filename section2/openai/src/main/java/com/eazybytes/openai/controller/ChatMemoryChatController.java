f741649f384b724d4dc35f909c2b634bbff8ce4d
Faruk Hossain <miloniitju@gmail.com>
2026-02-04 23:38:12 +0800

    Add InMemoryChatRepository with Advisor in ChatMemory

--- section2/openai/src/main/java/com/eazybytes/openai/controller/ChatMemoryChatController.java
+++ section2/openai/src/main/java/com/eazybytes/openai/controller/ChatMemoryChatController.java
@@ -3,10 +3,9 @@ package com.eazybytes.openai.controller;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.ai.chat.memory.ChatMemory.CONVERSATION_ID;
 
 @RestController
 @RequestMapping("/api")
@@ -19,10 +18,14 @@ public class ChatMemoryChatController {
     }
 
     @GetMapping("/chat-memory")
-    public ResponseEntity<String> chat(@RequestParam("message") String message) {
+    public ResponseEntity<String> chat(@RequestHeader("username") String username,
+                                       @RequestParam("message") String message) {
         return ResponseEntity.ok(chatClient
                 .prompt()
                 .user(message)
+                .advisors(
+                        advisorSpec -> advisorSpec.param(CONVERSATION_ID, username)
+                )
                 .call()
                 .content());
     }
